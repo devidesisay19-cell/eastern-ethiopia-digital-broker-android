94f4f9fd0f29cac7df8cac32b86401825d348170
dawit sisay <devidesisay19@gmail.com>
2026-09-15 21:49:34 +0300

    Fix Android Gradle build configuration and add Gradle wrapper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -31,9 +30,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
